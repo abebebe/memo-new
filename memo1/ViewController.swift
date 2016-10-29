f80583213ad183e063e3ac7a3177ab8965cf8ffd
nowall <nowall@nowall-no-MacBook-Air.local>
2016-10-29 17:41:35 +0900

    Initial Commit

---
+++ memo1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  memo1
+//
+//  Created by nowall on 2016/10/29.
+//  Copyright © 2016年 nonwall-hachimantai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
